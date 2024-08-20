128c46f79892d4c1f55932f79e559f998ec44a10
Binit kumar jha <jhabinit98@gmail.com>
2024-08-20 11:49:58 +0530

    changing favicon

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-// import React from "react";
-// import Header from "./Components/Header";
-// import Hero from "./Components/Hero";
-// import About from "./Components/About";
-// import Project from "./Components/Project";
-// import Skill from "./Components/Skill";
-// import Contact from "./Components/Contact";
-// import Footer from "./Components/Footer";
-
-
-
-// function App() {
-//   return (
-//     <>
-//       <main className="text-gray-400 bg-gray-900 body-font">
-//       <Header />
-//       <Hero/>
-//       <About />
-//       <Project />
-//       <Skill />
-//       <Contact/>
-//       <Footer />
-//     </main>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
 
 import React from "react";
 import Header from "./Components/Header";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import './About.css';
-// import binitImage from './binit.png';
-
-// const About = () => {
-//   return (
-//     <div className="about-container">
-//       {/* Photo of yourself */}
-//       <div className="photo">
-//         <img src={binitImage} alt="Binit Kumar Jha" />
-//       </div>
-
-//       {/* About me section */}
-//       <div className="about-me">
-//         <h1 className='heading'>About Me</h1>
-//         <p className='about'>
-//         Hello, I'm Binit Kumar Jha, a dedicated Computer Science and Engineering student currently in my 4th year at Maulana Abul Kalam Azad University of Technology, West Bengal. I am passionate about turning innovative ideas into reality through technology. My strong suit lies in data structures and algorithms, 
-//         and I'm always eager to expand my knowledge and take on new challenges in the world of computer science. 
-//         Feel free to connect with me to discuss opportunities or collaborate on exciting projects</p>
-//       </div>
-
-//       {/* Background image */}
-//       <div className="background-image">
-//         <img src="[your background image URL]" alt="Background Image" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
-
-
-
-
 
 import React from 'react';
 import './About.css';
